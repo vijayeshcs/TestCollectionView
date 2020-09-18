242d77383ac4c287028b7954b8c9a131309c0630
Vijayesh <esha@Eshas-MacBook-Pro.local>
2020-09-18 14:07:14 +0200

    Project created

---
+++ LoudlyCollectionView/LoudlyCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoudlyCollectionView
+//
+//  Created by Vijayesh on 18/09/20.
+//  Copyright © 2020 Vijayesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
